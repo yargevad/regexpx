a600f172b3f042ac1d138fafc3581c0ef2852059
Dave Gray <yargevad+github@gmail.com>
2017-01-26 16:26:13 -0700

    doc updates; rename Replace arguments; RegexpSet.ReplaceSubmatch and tests

--- regexpx.go
+++ regexpx.go
@@ -6,7 +6,7 @@ import (
 
 type RegexpSet []*regexp.Regexp
 
-// Match returns true on a match, along with the index of the matching pattern.
+// Match returns true if any expression matches s, along with the corresponding index.
 // When no matches are found, an index of -1 is returned.
 func (rs *RegexpSet) Match(s string) (bool, int) {
 	for idx, r := range []*regexp.Regexp(*rs) {
@@ -18,9 +18,8 @@ func (rs *RegexpSet) Match(s string) (bool, int) {
 }
 
 // Split slices s into all substrings separated by the first matching expression in the set.
-// It returns a slice of the substrings between the matches, along with the index of the matching pattern.
-// When called on an expression that contains no metacharacters, it is equivalent to strings.SplitN.
-// See also regexp.Split and strings.SplitN.
+// It returns a slice of the substrings between the matches, along with the corresponding index.
+// See also regexp.Split.
 func (rs *RegexpSet) Split(s string, n int) ([]string, int) {
 	for idx, r := range []*regexp.Regexp(*rs) {
 		if r.FindStringIndex(s) != nil {
@@ -30,14 +29,27 @@ func (rs *RegexpSet) Split(s string, n int) ([]string, int) {
 	return nil, -1
 }
 
-// Replace returns a copy of s, replacing matches of the first matching expression with r.
-// The index of the matching pattern is also returned, or -1 when no expression matches.
+// Replace returns a copy of s, replacing matches with the second argument, repl.
+// The index of the matching expression is also returned, or -1 when no expression matches.
 // See also regexp.ReplaceAllString
-func (rs *RegexpSet) Replace(s, replace string) (string, int) {
+func (rs *RegexpSet) Replace(s, repl string) (string, int) {
 	for idx, r := range []*regexp.Regexp(*rs) {
 		if r.FindStringIndex(s) != nil {
-			return r.ReplaceAllString(s, replace), idx
+			return r.ReplaceAllString(s, repl), idx
 		}
 	}
 	return s, -1
+}
+
+// ReplaceSubmatch returns a copy of s, replacing matches with the second argument, repl.
+// The index of the matching expression is also returned, or -1 when no expression matches.
+// Any submatches (aka capturing groups) are also returned
+// See also the Submatch section of the regexp docs and regexp.FindStringSubmatch
+func (rs *RegexpSet) ReplaceSubmatch(s, repl string) (string, int, []string) {
+	for idx, r := range []*regexp.Regexp(*rs) {
+		if m := r.FindStringSubmatch(s); m != nil {
+			return r.ReplaceAllString(s, repl), idx, m
+		}
+	}
+	return s, -1, nil
 }
--- regexpx_test.go
+++ regexpx_test.go
@@ -2,7 +2,6 @@ package regexpx_test
 
 import (
 	"regexp"
-	"strings"
 	"testing"
 
 	rx "github.com/yargevad/regexpx"
@@ -30,9 +29,9 @@ func TestMatch(t *testing.T) {
 	} {
 		actual, idx := testMatch.Match(test.Input)
 		if actual != test.Match {
-			t.Fatalf("string [%s] expected %t actual %t", test.Input, test.Match, actual)
+			t.Fatalf("string %q expected %t actual %t", test.Input, test.Match, actual)
 		} else if idx != test.Index {
-			t.Fatalf("string [%s] expected index %d, actual %d", test.Input, test.Index, idx)
+			t.Fatalf("string %q expected index %d, actual %d", test.Input, test.Index, idx)
 		}
 	}
 }
@@ -54,13 +53,13 @@ func TestSplit(t *testing.T) {
 	} {
 		out, idx := testSplit.Split(test.Input, -1)
 		if idx != test.Index {
-			t.Fatalf("string [%s] expected index %d, actual %d", test.Input, test.Index, idx)
+			t.Fatalf("string %q expected index %d, actual %d", test.Input, test.Index, idx)
 		} else if len(out) != len(test.Output) {
-			t.Fatalf("string [%s] expected length %d, actual %d", test.Input, len(test.Output), len(out))
+			t.Fatalf("string %q expected length %d, actual %d", test.Input, len(test.Output), len(out))
 		} else {
 			for i, elt := range out {
 				if test.Output[i] != elt {
-					t.Fatalf("string [%s] expected %q, actual %q", test.Input, test.Output, out)
+					t.Fatalf("string %q expected %q, actual %q", test.Input, test.Output, out)
 				}
 			}
 		}
@@ -89,9 +88,71 @@ func TestReplace(t *testing.T) {
 	} {
 		out, idx := testReplace.Replace(test.Input, test.Replace)
 		if idx != test.Index {
-			t.Fatalf("string [%s] expected index %d, actual %d", test.Input, test.Index, idx)
-		} else if strings.Compare(out, test.Output) != 0 {
-			t.Fatalf("string [%s] expected [%s], actual [%s]", test.Input, test.Output, out)
+			t.Fatalf("string %q expected index %d, actual %d", test.Input, test.Index, idx)
+		} else if test.Output != out {
+			t.Fatalf("string %q expected %q, actual %q", test.Input, test.Output, out)
+		}
+	}
+}
+
+type ReplaceSubmatchTest struct {
+	Input   string
+	Replace string
+	Index   int
+	Output  string
+	Matches []string
+}
+
+var testReplaceSubmatch = rx.RegexpSet{
+	regexp.MustCompile(`(cd)`),
+}
+
+func TestReplaceSubmatch(t *testing.T) {
+	for _, test := range []ReplaceSubmatchTest{
+		{"abcdef", "", 0, "abef", []string{"cd", "cd"}},
+	} {
+		out, idx, matches := testReplaceSubmatch.ReplaceSubmatch(test.Input, test.Replace)
+		if idx != test.Index {
+			t.Fatalf("string %q expected index %d, actual %d", test.Input, test.Index, idx)
+		} else if test.Output != out {
+			t.Fatalf("string %q expected %q, actual %q", test.Input, test.Output, out)
+		} else if len(test.Matches) != len(matches) {
+			t.Fatalf("string %q expected length %d, actual %d", test.Input, test.Matches, matches)
+		} else {
+			for i, m := range matches {
+				if test.Matches[i] != m {
+					t.Fatalf("string %q expected matches %q, actual %q", test.Input, test.Matches, matches)
+				}
+			}
+		}
+	}
+}
+
+var testSuffixes = rx.RegexpSet{
+	regexp.MustCompile(`(?i)\s*\b(jr\.?)(?:(,)|$)`),
+	regexp.MustCompile(`(?i)\s*\b(sr\.?)(?:(,)|$)`),
+	regexp.MustCompile(`(?i)\s*\b(iii?)(?:(,)|$)`),
+	regexp.MustCompile(`(?i)\s*\b(iv)(?:(,)|$)`),
+}
+
+func TestSuffixes(t *testing.T) {
+	for _, test := range []ReplaceSubmatchTest{
+		{"DOE JR, JOHN J", "$2", 0, "DOE, JOHN J", []string{" JR,", "JR", ","}},
+		{"Doe, Jane G Jr", "$2", 0, "Doe, Jane G", []string{" Jr", "Jr", ""}},
+	} {
+		out, idx, matches := testSuffixes.ReplaceSubmatch(test.Input, test.Replace)
+		if idx != test.Index {
+			t.Fatalf("string %q expected index %d, actual %d", test.Input, test.Index, idx)
+		} else if test.Output != out {
+			t.Fatalf("string %q expected %q, actual %q", test.Input, test.Output, out)
+		} else if len(test.Matches) != len(matches) {
+			t.Fatalf("string %q expected length %d, actual %d", test.Input, len(test.Matches), len(matches))
+		} else {
+			for i, m := range matches {
+				if test.Matches[i] != m {
+					t.Fatalf("string %q expected matches %q, actual %q", test.Input, test.Matches, matches)
+				}
+			}
 		}
 	}
 }
